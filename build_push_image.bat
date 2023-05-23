f2eeea8feee31fa5c14d20d9b78c88ea192478e8
ArifAkasah <78593179+ArifAkasah@users.noreply.github.com>
2023-05-24 07:51:49 +0800

    Update build_push_image.bat

--- build_push_image.bat
+++ build_push_image.bat
@@ -11,7 +11,7 @@ docker tag item-app:v1 docker.pkg.github.com/arifakasah/item-app/item-app:v1
 @REM # Memberi nama baru pada Docker image "item-app:v1" dengan nama "docker.pkg.github.com/arifakasah/item-app/item-app:v1" untuk menyesuaikan dengan nama repository di GitHub Packages.
 
 @REM # Instruksi 4.
-docker login docker.pkg.github.com -u ArifAkasah -p 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
+docker login docker.pkg.github.com -u ArifAkasah -p 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
 @REM # Login ke GitHub Packages dengan mengirimkan nama pengguna dan token akses pribadi sebagai argumen. 
 
 @REM # Instruksi 5.
